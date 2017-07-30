a705c8420a046917688f2d336afcc2bae45ddfcb
alicespyglass <alicespyglass@gmail.com>
2017-07-30 17:05:43 +0100

    home.component - link templateUrl

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    template: `<h1>{{welcome}}</h1>`
+    templateUrl:
+    './home.component.html'
 })
+
 export class HomeComponent {
-    welcome : string;
-    constructor(){
-        this.welcome = "Welcome to home page";
-    };
+
+
+
 };
