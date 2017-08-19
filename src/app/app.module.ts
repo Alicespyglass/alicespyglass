45548c8e276cb4c62b329b4636e09461e7b932a8
alicespyglass <alicespyglass@gmail.com>
2017-08-19 21:22:47 +0100

    reset - import html template. Upload gameboy profile photo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgModule }      from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { routing }      from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     NgbModule.forRoot(),
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component'; //import home components
-
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent, pathMatch: 'full'} // redirect to home page on load
 ];
 
