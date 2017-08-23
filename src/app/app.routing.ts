aaa7967ed757b5f6115239319f0bf1876911d84f
Alice Cheung <alice@myurbanjungle.com>
2017-08-23 17:20:12 +0100

    work at UJ

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,12 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full'} // redirect to home page on load
+  {
+    //   path: '',
+    //   component: HomeComponent,
+    //   pathMatch: 'full'
+  } // redirect to home page on load
 ];
+
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
